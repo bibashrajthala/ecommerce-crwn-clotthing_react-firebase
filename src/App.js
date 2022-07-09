846e64f37b52b276b004de1eac2f2976dae6bf7f
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-10 00:41:35 +0545

    routing,nested-route,outlet

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import Home from "./routes/home/home.component";
 
-  return <Directory categories={categories} />;
-}
+const Shop = () => {
+  return <h1>this is the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
