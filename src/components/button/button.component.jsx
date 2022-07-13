e452d9ac421196f0e4903cbd9348de4c13ef9906
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-14 03:43:26 +0545

    change sign-in to authentication route, create separate sign-in component and implement sign-in authentication, firebase authentication section complete

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -7,7 +7,10 @@ const BUTTON_TYPE_CLASSES = {
 
 const Button = ({ children, label, buttonType, ...otherProps }) => {
   return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}>
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
       {children ? children : label}
     </button>
   );
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -7,7 +7,7 @@ const FormInput = ({ label, ...otherProps }) => {
       <input {...otherProps} className="form-input" />
       {label && (
         <label
-          htmlFor={otherProps.name}
+          htmlFor={otherProps.id}
           className={`${
             otherProps.value.length ? "shrink" : ""
           } form-input-label`}
