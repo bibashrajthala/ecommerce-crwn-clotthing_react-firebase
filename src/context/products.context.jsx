add607d7f342b37fd6b610ebf28736f05d4c9641
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-24 14:42:25 +0545

    add SHOP_DATA.js's array data to categories collection and documents in Firestore

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,14 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import PRODUCTS from "../shop-data.json";
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products };
+
+  useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);
 
   return (
     <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,14 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDRuhTpXTXbiknt4QZ7X4SGmDoUlipVlFM",
@@ -39,6 +46,24 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    console.log(object);
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
