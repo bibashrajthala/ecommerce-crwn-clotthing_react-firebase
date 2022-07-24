1021d2bb05bea19c7aeb64f295b7522c5ac26eec
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-24 21:35:23 +0545

    fix clashing styles,ie. resolve styles conflicts

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import { CategoriesContext } from "../../context/categories.context";
@@ -17,12 +17,15 @@ const Category = () => {
   }, [categoriesMap, category]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
