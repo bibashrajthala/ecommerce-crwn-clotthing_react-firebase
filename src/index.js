0381852b61625ca62f41b8daf3098b652b4fea95
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-08-10 13:34:46 +0545

    used redux-persist library to persist redux store's state in local storage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -14,9 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* can pass a component to show loading in loading prop */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,10 @@ import {
   applyMiddleware,
 } from "redux";
 
+// persist the redux reducers and redux store ie states using local storage(by default , you can change it in config though)
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 // import logger from "redux-logger";
 // logger is optional to use, it helps to see what state looks like before action is dispached ,what action was dispatched, and   what state looks like after the aciton is dispatched to reducer
 // lets use logger as middleware
@@ -35,8 +39,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
 const middlewares = [loggerMiddleware]; // middleware is something that receives action before it is sent to reducers // can use multiple middleware as well as element of middlewares array
 const composedEnhancers = compose(applyMiddleware(...middlewares)); // a middleware is a type of store enhancer among many, enhancers enhances the store
 
+const persistConfig = {
+  key: "root", // start persisting from root , ie persist everything
+  storage, // storage: storage, by  default uses local storage of browser, can change though
+  blacklist: ["user"], // include reducer(slice) whose state  that you dont want to persist here, user data is already persisted bt  firebase auth listener so no need to clash its persistance with redux persist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // created a persisted root reducer(excluding the blaclisted reducers)
+
 export const store = createStore(
-  rootReducer, // necessary parameter => the combination of all reducers ,ie it is what makes store storing the states returned of all reducers
+  persistedReducer, // necessary parameter => the combination of all reducers(here used persisted one) ,ie it is what makes store storing the states returned of all reducers
   undefined, // optional parameter=> default state in store , if necessary h for now we dont need it so used undefined
   composedEnhancers // optional parameter => here used middleware
 );
+
+export const persistor = persistStore(store); // created a persited store
+
+// need to provide this persisted store to our app in index.js
