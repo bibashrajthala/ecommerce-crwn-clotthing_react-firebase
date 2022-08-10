92c0a11b0b491e8035515a69782ee9892e0662ce
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-08-10 14:04:27 +0545

    implement using redux devtool extension

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,23 @@
+// making our own middleware that logs action and state instead of using react-logger
+export const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("action type", action.type);
+  console.log("action payload", action.payload);
+  console.log(
+    "currentState(state before action was dispatched to reducers)",
+    store.getState()
+  );
+
+  next(action); // sending/dispatching action to next middleware if present or to reducers if no next middleware is present
+
+  // each time a action is dipatched to reducers and a reducer update state( in rootreducer of) store,all the selectors get fired then only the code below next() will run then after that each component using useSelector() will also re-render as selector got fired again
+
+  console.log(
+    "nextState(state after action was dispatched to reducers)",
+    store.getState()
+  );
+};
+
+// you can use this instead of logger we are using from redux-logger
--- src/store/store.js
+++ src/store/store.js
@@ -3,56 +3,37 @@ import {
   compose,
   applyMiddleware,
 } from "redux";
-
-// persist the redux reducers and redux store ie states using local storage(by default , you can change it in config though)
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
-// import logger from "redux-logger";
-// logger is optional to use, it helps to see what state looks like before action is dispached ,what action was dispatched, and   what state looks like after the aciton is dispatched to reducer
-// lets use logger as middleware
+import logger from "redux-logger";
 
 import { rootReducer } from "./root.reducer";
 
-// making our own middleware that logs action and state instead of using react-logger
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("action type", action.type);
-  console.log("action payload", action.payload);
-  console.log(
-    "currentState(state before action was dispatched to reducers)",
-    store.getState()
-  );
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+); // only user logger when in development and not in production ,ie production app should log our store states
 
-  next(action); // sending/dispatching action to next middleware if present or to reducers if no next middleware is present
-
-  // each time a action is dipatched to reducers and a reducer update state( in rootreducer of) store,all the selectors get fired then only the code below next() will run then after that each component using useSelector() will also re-render as selector got fired again
-
-  console.log(
-    "nextState(state after action was dispatched to reducers)",
-    store.getState()
-  );
-};
-
-const middlewares = [loggerMiddleware]; // middleware is something that receives action before it is sent to reducers // can use multiple middleware as well as element of middlewares array
-const composedEnhancers = compose(applyMiddleware(...middlewares)); // a middleware is a type of store enhancer among many, enhancers enhances the store
+const composeEnhancer =
+  (process.env.NODE_ENV &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+// use redux devtool extension's compose in browser window if availble, otherwise use redux's compose
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 const persistConfig = {
-  key: "root", // start persisting from root , ie persist everything
-  storage, // storage: storage, by  default uses local storage of browser, can change though
-  blacklist: ["user"], // include reducer(slice) whose state  that you dont want to persist here, user data is already persisted bt  firebase auth listener so no need to clash its persistance with redux persist
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
-
-const persistedReducer = persistReducer(persistConfig, rootReducer); // created a persisted root reducer(excluding the blaclisted reducers)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(
-  persistedReducer, // necessary parameter => the combination of all reducers(here used persisted one) ,ie it is what makes store storing the states returned of all reducers
-  undefined, // optional parameter=> default state in store , if necessary h for now we dont need it so used undefined
-  composedEnhancers // optional parameter => here used middleware
+  persistedReducer,
+  undefined,
+  composedEnhancers
 );
 
-export const persistor = persistStore(store); // created a persited store
+export const persistor = persistStore(store);
 
 // need to provide this persisted store to our app in index.js
