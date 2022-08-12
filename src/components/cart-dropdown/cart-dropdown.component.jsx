f0185c68c4481b8373a9fc92be0cfc93319cda82
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-08-12 15:41:39 +0545

    implement useCallback

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -16,18 +17,28 @@ const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => navigate("/checkout");
+  const [count, setCount] = useState(0);
+
+  // useCallback is used to memoize whole funciton(not just the returned value of function , but the whole func ) so that there is no extra unnecssary initilazation and running of funciton during re-render of component
+  // ie the callback function inside of useCallback only re-initialize and re-run only if there is some change in function otherwise , it always runs the same memoized function
+  const goToCheckoutHandler = useCallback(() => {
+    console.log(count);
+    // navigate("/checkout");
+  }, [count]);
+
+  const goToUpdateHandler = () => setCount(count + 1);
 
   return (
     <CartDropdownContainer>
-      <CartItems>
+      {/*<CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
           <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </CartItems>
+        </CartItems>*/}
       <Button label="Go To Checkout" onClick={goToCheckoutHandler} />
+      <Button label="Update count" onClick={goToUpdateHandler} />
     </CartDropdownContainer>
   );
 };
